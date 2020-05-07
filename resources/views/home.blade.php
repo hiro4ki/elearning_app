3b31b4363364d8f52adc14de3a7870fb06d0efde
hkato-intern <philippines-internship-003@sun-asterisk.com>
2020-05-07 16:54:44 +0900

    Dashboard Page Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,70 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class=" row">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card">
+                <img class="card-img-top" src="" alt="place image">
+                <div class="card-body bg-light p-2">
+                    <h6 class="card-title">
+                        Prace
+                    </h6>
+                    <p class="card-text">
+                        Describe
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card">
+                <img class="card-img-top" src="" alt="place image">
+                <div class="card-body bg-light p-2">
+                    <h6 class="card-title">
+                        Prace
+                    </h6>
+                    <p class="card-text">
+                        Describe
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card">
+                <img class="card-img-top" src="" alt="place image">
+                <div class="card-body bg-light p-2">
+                    <h6 class="card-title">
+                        Prace
+                    </h6>
+                    <p class="card-text">
+                        Describe
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-md-6 col-sm-12">
+            <div class="card">
+                <img class="card-img-top" src="" alt="place image">
+                <div class="card-body bg-light p-2">
+                    <h6 class="card-title">
+                        Prace
+                    </h6>
+                    <p class="card-text">
+                        Describe
+                    </p>
                 </div>
             </div>
         </div>
