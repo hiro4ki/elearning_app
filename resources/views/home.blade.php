21902af5da3bf0e70f5cbc6d47cf34faa41ce359
hkato-intern <philippines-internship-003@sun-asterisk.com>
2020-05-11 14:35:20 +0900

    modify home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,6 @@
 @section('content')
 
 <div class="container">
-    @if (auth()->user()->is_admin) {{-- admin user--}}
-    @include('admin/users')
-    @else {{-- normal user --}}
     @include('normal_users/user')
-    @endif
 </div>
 @endsection
